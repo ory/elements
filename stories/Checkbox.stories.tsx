6ffd8490871e1401c5c0ab231fd40fedd683a006
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-10-02 19:54:26 +0200

    feat: implement checkbox component

---
+++ stories/Checkbox.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import { Story } from '@storybook/react/types-6-0';
+import { Container, Spacer } from './storyhelper';
+import Checkbox, {CheckboxProps} from './Checkbox';
+
+const meta: Meta = {
+  title: 'Checkbox',
+  component: Checkbox
+};
+
+const Template: Story<CheckboxProps> = (args: CheckboxProps) => (
+  <Container>
+    <Checkbox {...args} />
+  </Container>
+);
+
+export const Playground = Template.bind({});
+Playground.args = {
+  label: 'Send me occasional updates and announcements with newsletters'
+};
+
+export const CheckboxWithTitleDescription = () => (
+  <Container>
+    <Spacer>
+      <Checkbox
+        title="Preferences"
+        label="Send me occasional updates and announcements with newsletters" />
+    </Spacer>
+  </Container>
+);
+
+
+export default meta;
