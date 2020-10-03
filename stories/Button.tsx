e150896b360b56f7488cba350a4dc55d899dd0ee
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-10-03 18:14:08 +0200

    fix: resolve type errors

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -2,8 +2,6 @@ import React, { ReactNode } from 'react';
 import styled from 'styled-components';
 import { buttonStyles, ButtonStyles } from '../theme';
 
-const StyledButton = styled.input(buttonStyles);
-
 export interface ButtonProps
   extends React.InputHTMLAttributes<HTMLInputElement>,
     ButtonStyles {
@@ -11,16 +9,16 @@ export interface ButtonProps
   children: string;
 }
 
-const Button = ({ helper, children, ...props }: ButtonProps) => (
-  <>
-    <StyledButton
+const Button = ({ helper, children, className, ...props }: ButtonProps) => (
+  <div className={className}>
+    <input
       type="button"
       className="button"
       {...props}
       value={children}
     />
     {helper && <span className="button-helper">{helper}</span>}
-  </>
+  </div>
 );
 
-export default Button;
+export default styled(Button)(buttonStyles);
--- theme/buttonStyles.ts
+++ theme/buttonStyles.ts
@@ -7,38 +7,42 @@ export interface ButtonStyles extends ThemeProps {
 export const buttonStyles = ({ big, theme }: ButtonStyles) => `
 ${typographyButtonStyles({ theme })}
 
-line-height: ${!big ? '20px' : '30px'};
-color: ${theme.grey0};
-border-radius: ${theme.borderRadius};
+& .button {
+  line-height: ${!big ? '20px' : '30px'};
+  color: ${theme.grey0};
+  border-radius: ${theme.borderRadius};
+  
+  width: 100%;
+  
+  padding: 5px 12px;
+  margin: 7px 0;
+  border: 2px solid transparent;
+  outline: none;
+  
+  background-color: ${theme.primary60};
+ 
+  cursor: pointer;
+}
 
-width: 100%;
-
-padding: 5px 12px;
-margin: 7px 0;
-border: 2px solid transparent;
-outline: none;
-
-background-color: ${theme.primary60};
-
-&:disabled, &:hover:disabled {
+& .button:disabled, & .button:hover:disabled {
   color: ${theme.grey30};
   background-color: ${theme.grey10};
 }
 
-&.fake-hover,
-&:hover {
+& .button.fake-hover,
+& .button:hover {
   background-color: ${theme.primary30};
 }
 
-&.fake-focus,
-&:focus {
+& .button.fake-focus,
+& .button:focus {
   background-color: ${theme.primary60};
   border: 2px solid ${theme.blue30};
   outline: none;
 }
 
-&.fake-click,
-&:active {
+& .button.fake-click,
+& .button:active {
   background-color: ${theme.primary70};
   outline: none;
   border: 2px solid transparent;
