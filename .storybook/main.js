5aac5202c4db40232a38afb218ebdffd628dced4
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-09-02 08:37:03 +0300

    feat: restructure to accommodate new build chain

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)",
-    "../stories/*.stories.@(js|jsx|ts|tsx|mdx)"
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx|mdx)",
+    "../src/stories/*.stories.@(js|jsx|ts|tsx|mdx)"
   ],
   "addons": [
     "@storybook/addon-links",
