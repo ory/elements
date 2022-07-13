ce9c52379434e2268db11ed59c18283d6efe0e29
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-07-13 18:14:47 +0200

    fix: social button border radius

--- src/theme/socialButtonStyles.ts
+++ src/theme/socialButtonStyles.ts
@@ -14,7 +14,7 @@ export const socialButtonStyles = ({theme, size}: ButtonStyles) => {
 & .button {
   ${typographyButtonStyles({theme}, size)}
   color: ${theme.palettes.light.foreground.muted};
-  border-radius: ${theme.cards.borderRadius};
+  border-radius: ${theme.buttons.borderRadius};
   
   width: 100%;
   min-height: ${pxToRem(size === 'large' ? 64 : size === 'small' ? 40 : 48)};
