8c59490dc1f9802e8d1f48e716214c0d59fd4ae1
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-07-26 11:03:15 +0200

    fix: standard html wrapper

---
+++ src/html/component-wrapper.ts
@@ -0,0 +1,6 @@
+import ReactDOMServer from 'react-dom/server';
+import {ReactElement} from "react";
+
+export const ComponentWrapper = (children: ReactElement) => {
+  return ReactDOMServer.renderToStaticMarkup(children);
+}
