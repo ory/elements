8bce049eab74c549ca18b0c2ae9f07202268807a
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-08-05 12:06:47 +0200

    feat: ory auth card

--- src/react/ory/FlowTypes.ts
+++ src/react/ory/FlowTypes.ts
@@ -13,29 +13,6 @@ export type SelfServiceFlow =
   | SelfServiceSettingsFlow
   | SelfServiceVerificationFlow;
 
-export type SelfServiceFlowGroup =
-  | 'password'
-  | 'default'
-  | 'profile'
-  | 'oidc'
-  | 'link'
-  | 'webauthn'
-  | 'totp'
-  | 'lookup_secret';
-
-// TODO remove this type once the SDK is updated and has this enum
-export type UiNodeInputType =
-  | 'button'
-  | 'submit'
-  | 'text'
-  | 'password'
-  | 'email'
-  | 'hidden'
-  | 'script'
-  | 'input'
-  | 'checkbox'
-  | 'datetime-local';
-
 export type ErrorProps = {
   code: number;
   details: {
