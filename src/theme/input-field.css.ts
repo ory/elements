508eae72246152c8b00a515355b90aee0f91309c
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-08-08 15:34:22 +0200

    fix: card

--- src/theme/input-field.css.ts
+++ src/theme/input-field.css.ts
@@ -7,22 +7,25 @@ export const inputFieldTitleStyle = style({
 });
 
 export const inputFieldStyle = style({
+  all: 'unset',
+  boxSizing: 'border-box',
   color: oryTheme.input.text,
   background: oryTheme.input.background,
   border: `1px solid ${oryTheme.border.default}`,
   borderRadius: pxToRem(4),
   padding: pxToRem(12, 24),
+  ':hover': {
+    border: `1px solid ${oryTheme.accent.muted}`
+  },
   ':focus': {
-    border: `4px solid ${oryTheme.accent.muted}`
+    inset: `4px`,
+    borderColor: oryTheme.accent.muted
   },
   ':active': {
     border: `1px solid ${oryTheme.accent.emphasis}`
   },
   ':invalid': {
     border: `1px solid ${oryTheme.error.emphasis}`
-  },
-  ':valid': {
-    border: `1px solid ${oryTheme.success.emphasis}`
   },
   ':disabled': {
     border: `1px solid ${oryTheme.input.disabled}`
