f68062f41686a26de8570d03beda64d2edf38ce8
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-08-16 08:50:59 +0200

    u

--- src/react-components/ory/node-script.tsx
+++ src/react-components/ory/node-script.tsx
@@ -1,32 +1,35 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { UiNode, UiNodeScriptAttributes } from '@ory/client';
 import { HTMLAttributeReferrerPolicy } from 'react';
 import { filterNodesByGroups } from '@ory/integrations/ui';
 
-export const ScriptNodes = ({ nodes }: { nodes: UiNode[] }) => {
-  const scriptNodes = filterNodesByGroups({
-    nodes: nodes,
-    withoutDefaultGroup: true,
-    groups: ['webauthn'],
-    attributes: ['script']
-  }).map((node) => node.attributes as UiNodeScriptAttributes);
+export const useScriptNodes = ({ nodes }: { nodes: UiNode[] }) => {
+  useEffect(() => {
+    const scriptNodes = filterNodesByGroups({
+      nodes: nodes,
+      withoutDefaultGroup: true,
+      withoutDefaultAttributes: true,
+      groups: ['webauthn'],
+      attributes: ['script']
+    }).map((node) => {
+      const attr = node.attributes as UiNodeScriptAttributes;
+      const script = document.createElement('script');
+      script.setAttribute('data-testid', `node/script/${script.id}`);
+      script.src = attr.src;
+      script.type = attr.type;
+      script.async = attr.async;
+      script.referrerPolicy =
+        attr.referrerpolicy as HTMLAttributeReferrerPolicy;
+      script.crossOrigin = attr.crossorigin;
+      script.integrity = attr.integrity;
+      document.body.appendChild(script);
+      return script;
+    });
 
-  console.log({ scriptNodes });
-
-  return (
-    <>
-      {scriptNodes.map((script) => (
-        <script
-          key={script.id}
-          type={script.type}
-          src={script.src}
-          integrity={script.integrity}
-          referrerPolicy={script.referrerpolicy as HTMLAttributeReferrerPolicy}
-          crossOrigin={script.crossorigin}
-          data-testid={`node/script/${script.id}`}
-          {...(script.async && { async: true })}
-        />
-      ))}
-    </>
-  );
+    return () => {
+      scriptNodes.forEach((script) => {
+        document.body.removeChild(script);
+      });
+    };
+  }, [nodes]);
 };
