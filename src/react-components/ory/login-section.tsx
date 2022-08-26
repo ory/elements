6d06c64bb529b71cb993968d23c1a14c010f9f24
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-08-26 18:09:13 +0200

    fix: elements when password is disabled

--- src/react-components/ory/login-section.tsx
+++ src/react-components/ory/login-section.tsx
@@ -4,6 +4,7 @@ import { gridStyle } from "../../theme"
 import { ButtonLink } from "../button-link"
 import { Divider } from "../divider"
 import { FilterFlowNodes } from "./filter-flow-nodes"
+import { filterNodesByGroups } from "@ory/integrations/ui"
 
 export type LoginSectionProps = {
   nodes: UiNode[]
@@ -15,44 +16,55 @@ export const LoginSection = ({
   nodes,
   forgotPasswordURL,
   isLoggedIn,
-}: LoginSectionProps): JSX.Element => {
-  return isLoggedIn ? (
-    <div className={gridStyle({ gap: 32 })}>
-      <Divider />
-      <FilterFlowNodes
-        filter={{
-          nodes: nodes,
-          excludeAttributes: "hidden",
-        }}
-      />
-    </div>
-  ) : (
-    <div className={gridStyle({ gap: 32 })}>
-      <Divider />
-      <div className={gridStyle({ gap: 16 })}>
+}: LoginSectionProps): JSX.Element | null => {
+  const hasPassword =
+    filterNodesByGroups({
+      nodes,
+      groups: "password",
+      withoutDefaultGroup: true,
+      withoutDefaultAttributes: true,
+      attributes: "submit",
+    }).length > 0
+
+  return hasPassword ? (
+    isLoggedIn ? (
+      <div className={gridStyle({ gap: 32 })}>
+        <Divider />
         <FilterFlowNodes
           filter={{
             nodes: nodes,
-            groups: ["default", "password"],
-            excludeAttributes: ["submit", "hidden"],
+            excludeAttributes: "hidden",
           }}
         />
-        {forgotPasswordURL && (
-          <ButtonLink
-            data-testid="forgot-password-link"
-            href={forgotPasswordURL}
-          >
-            Forgot Password?
-          </ButtonLink>
-        )}
       </div>
-      <FilterFlowNodes
-        filter={{
-          nodes: nodes,
-          groups: ["password"],
-          attributes: "submit",
-        }}
-      />
-    </div>
-  )
+    ) : (
+      <div className={gridStyle({ gap: 32 })}>
+        <Divider />
+        <div className={gridStyle({ gap: 16 })}>
+          <FilterFlowNodes
+            filter={{
+              nodes: nodes,
+              groups: ["default", "password"],
+              excludeAttributes: ["submit", "hidden"],
+            }}
+          />
+          {forgotPasswordURL && (
+            <ButtonLink
+              data-testid="forgot-password-link"
+              href={forgotPasswordURL}
+            >
+              Forgot Password?
+            </ButtonLink>
+          )}
+        </div>
+        <FilterFlowNodes
+          filter={{
+            nodes: nodes,
+            groups: ["password"],
+            attributes: "submit",
+          }}
+        />
+      </div>
+    )
+  ) : null
 }
