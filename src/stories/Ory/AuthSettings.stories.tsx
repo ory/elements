c1fcac622a54eb2204db9aa64625bda0ff26c33f
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-09-27 11:46:24 +0200

    fix: conditional settings rendering

---
+++ src/stories/Ory/AuthSettings.stories.tsx
@@ -0,0 +1,57 @@
+import React from "react"
+import { ComponentMeta, Story } from "@storybook/react"
+import { UserSettingsCard, UserSettingsCardProps } from "../../react-components"
+import { Container } from "../storyhelper"
+
+import settingsFlow from "./settings-flow.json"
+import { SelfServiceSettingsFlow } from "@ory/client"
+
+export default {
+  title: "Ory/UserSettingsCard",
+  component: UserSettingsCard,
+} as ComponentMeta<typeof UserSettingsCard>
+
+const Template: Story<UserSettingsCardProps> = (
+  args: UserSettingsCardProps,
+) => (
+  <Container>
+    <div style={{ width: "400px" }}>
+      <UserSettingsCard {...args} />
+    </div>
+  </Container>
+)
+
+export const UserSettingsProfileCard = Template.bind({})
+
+UserSettingsProfileCard.args = {
+  flowType: "profile",
+  flow: settingsFlow as SelfServiceSettingsFlow,
+}
+
+export const UserSettingsPasswordCard = Template.bind({})
+
+UserSettingsPasswordCard.args = {
+  flowType: "password",
+  flow: settingsFlow as SelfServiceSettingsFlow,
+}
+
+export const UserSettingsWebauthnCard = Template.bind({})
+
+UserSettingsWebauthnCard.args = {
+  flowType: "webauthn",
+  flow: settingsFlow as SelfServiceSettingsFlow,
+}
+
+export const UserSettingsTotpCard = Template.bind({})
+
+UserSettingsTotpCard.args = {
+  flowType: "totp",
+  flow: settingsFlow as SelfServiceSettingsFlow,
+}
+
+export const UserSettingsOidcCard = Template.bind({})
+
+UserSettingsOidcCard.args = {
+  flowType: "oidc",
+  flow: settingsFlow as SelfServiceSettingsFlow,
+}
