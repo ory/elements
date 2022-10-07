51748b7bce2fe41b35b0a74e6c60afee20825707
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-10-07 16:19:39 +0200

    fix: gradient card overlay

--- src/react-components/card-gradient.tsx
+++ src/react-components/card-gradient.tsx
@@ -1,16 +1,18 @@
 import React from "react"
 import cn from "classnames"
 import {
+  cardContainer,
   cardGradientActionStyle,
+  cardGradientOverlayStyle,
   cardGradientStyle,
 } from "../theme/card-gradient.css"
-import { gridStyle, typographyStyle } from "../theme"
+import { colorSprinkle, gridStyle, typographyStyle } from "../theme"
 
 export interface CardGradientProps
-  extends React.HTMLAttributes<HTMLDivElement> {
+  extends React.HTMLAttributes<HTMLAnchorElement> {
   heading: string | React.ReactNode
   content: string | React.ReactNode
-  action: string | React.ReactNode
+  action: string
   target?: string
   disabled?: boolean
   className?: string
@@ -26,32 +28,41 @@ export const CardGradient = ({
   className,
   ...props
 }: CardGradientProps): JSX.Element => (
-  <div
-    className={cn(cardGradientStyle({ disabled: disabled }), className)}
-    {...props}
-  >
-    <div className={gridStyle({ gap: 16 })}>
-      {typeof heading === "string" ? (
-        <div className={typographyStyle({ size: "headline21", type: "bold" })}>
-          {heading}
-        </div>
-      ) : (
-        heading
+  <div className={cn(cardContainer, cardGradientOverlayStyle({ disabled }))}>
+    <a
+      className={cn(
+        cardGradientStyle,
+        colorSprinkle({
+          color: disabled ? "foregroundOnDisabled" : "foregroundOnDark",
+        }),
+        className,
       )}
-      {typeof content === "string" ? (
-        <div className={typographyStyle({ size: "small", type: "regular" })}>
-          {content}
-        </div>
-      ) : (
-        content
-      )}
-    </div>
-    {typeof action === "string" ? (
-      <a className={cardGradientActionStyle} href={action} target={target}>
-        <i className={cn(`fa fa-arrow-right`)}></i>
-      </a>
-    ) : (
-      action
-    )}
+      target={target}
+      aria-disabled={disabled}
+      {...(!disabled && { href: action })}
+      {...props}
+    >
+      <div className={cn(gridStyle({ gap: 16 }))}>
+        {typeof heading === "string" ? (
+          <div
+            className={typographyStyle({ size: "headline21", type: "bold" })}
+          >
+            {heading}
+          </div>
+        ) : (
+          heading
+        )}
+        {typeof content === "string" ? (
+          <div className={typographyStyle({ size: "small", type: "regular" })}>
+            {content}
+          </div>
+        ) : (
+          content
+        )}
+      </div>
+    </a>
+    <i
+      className={cn(`fa fa-arrow-right`, cardGradientActionStyle({ disabled }))}
+    ></i>
   </div>
 )
--- src/theme/card-gradient.css.ts
+++ src/theme/card-gradient.css.ts
@@ -4,54 +4,109 @@ import { pxToRem } from "../common"
 import { defaultBreakpoints } from "./consts"
 import { oryTheme } from "./theme.css"
 
-export const cardGradientStyle = recipe({
-  base: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "stretch",
-    border: `1px solid ${oryTheme.border.def}`,
-    borderRadius: pxToRem(4),
-    padding: pxToRem(24),
-    background: oryTheme.accent.def,
-    color: oryTheme.foreground.onDark,
-    minHeight: pxToRem(312),
-    filter:
-      "drop-shadow(0px 0px 1px rgba(48, 49, 51, 0.05)) drop-shadow(0px 16px 24px rgba(48, 49, 51, 0.1))",
-    backgroundImage:
-      "linear-gradient(180deg, rgba(48, 48, 48, 0.85) 1.22%, rgba(48, 48, 48, 0.0001) 100%)",
-    maxWidth: pxToRem(209),
-    minWidth: pxToRem(209),
-    "@media": {
-      [`screen and (max-width: ${defaultBreakpoints.xs})`]: {
-        width: "100%",
-        border: "0px",
-        borderRadius: "0px",
-        padding: `0px 0px ${pxToRem(32)}`,
-        maxWidth: pxToRem(294),
-        minWidth: pxToRem(294),
-      },
+export const cardContainer = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "stretch",
+  border: `1px solid ${oryTheme.border.def}`,
+  cursor: "pointer",
+  maxWidth: pxToRem(209),
+  minWidth: pxToRem(209),
+  minHeight: pxToRem(312),
+  borderRadius: pxToRem(4),
+  padding: pxToRem(24),
+  filter:
+    "drop-shadow(0px 0px 1px rgba(48, 49, 51, 0.05)) drop-shadow(0px 8px 16px rgba(48, 49, 51, 0.1))",
+  "@media": {
+    [`screen and (max-width: ${defaultBreakpoints.xs})`]: {
+      width: "100%",
+      border: "0px",
+      borderRadius: "0px",
+      padding: `0px 0px ${pxToRem(32)}`,
+      maxWidth: pxToRem(294),
+      minWidth: pxToRem(294),
     },
   },
+})
+
+export const cardGradientOverlayStyle = recipe({
+  base: {
+    background: oryTheme.accent.def,
+  },
+  defaultVariants: { disabled: false },
   variants: {
     disabled: {
       true: {
         background: oryTheme.foreground.disabled,
       },
+      false: {
+        selectors: {
+          "&:before": {
+            content: " ",
+            display: "block",
+            position: "absolute",
+            opacity: "0.5",
+            top: 0,
+            left: 0,
+            background:
+              "linear-gradient(180deg, rgba(48, 48, 48, 0.85) 1.22%, rgba(48, 48, 48, 0.0001) 100%)",
+            backgroundSize: "cover",
+          },
+          "&:hover:before": {
+            content: " ",
+            display: "block",
+            position: "absolute",
+            opacity: "0.9",
+            width: "100%",
+            height: "100%",
+            top: 0,
+            left: 0,
+            background:
+              "linear-gradient(180deg, rgba(48, 48, 48, 0.85) 1.22%, rgba(48, 48, 48, 0.0001) 100%)",
+            backgroundSize: "cover",
+          },
+        },
+      },
     },
   },
 })
 
-export const cardGradientActionStyle = style({
-  marginRight: "auto",
-  marginTop: "auto",
-  width: pxToRem(40),
-  height: pxToRem(40),
-  padding: 0,
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  cursor: "pointer",
+export const cardGradientStyle = style({
   textDecoration: "none",
-  fontSize: pxToRem(27),
-  color: oryTheme.foreground.onAccent,
+  selectors: {
+    "&[aria-disabled='true']": {
+      cursor: "default",
+    },
+  },
+})
+
+export const cardGradientActionStyle = recipe({
+  base: {
+    marginRight: "auto",
+    marginTop: "auto",
+    width: pxToRem(40),
+    height: pxToRem(40),
+    padding: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textDecoration: "none",
+    fontSize: pxToRem(27),
+    color: oryTheme.foreground.onAccent,
+  },
+  defaultVariants: { disabled: false },
+  variants: {
+    disabled: {
+      true: {
+        color: oryTheme.foreground.onDisabled,
+      },
+      false: {
+        selectors: {
+          [`${cardContainer}:hover &`]: {
+            width: pxToRem(48),
+          },
+        },
+      },
+    },
+  },
 })
