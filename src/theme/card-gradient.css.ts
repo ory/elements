533199515785bcd6a5589ca35a82bf0460a77310
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2022-10-14 11:21:14 +0200

    fix: missing properties

--- src/theme/card-gradient.css.ts
+++ src/theme/card-gradient.css.ts
@@ -49,6 +49,8 @@ export const cardGradientOverlayStyle = recipe({
             display: "block",
             position: "absolute",
             opacity: "0.5",
+            width: "100%",
+            height: "100%",
             top: 0,
             left: 0,
             background:
