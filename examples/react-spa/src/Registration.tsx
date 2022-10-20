66164725ebb08ea661da57a2cf9aeb5d986d3ba3
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-10-20 09:28:34 +0200

    chore: update codedocs on react example (#31)
    * chore: update codedocs on react example
    
    * chore: cleanup recovery component
    
    * chore: update settings flow
    
    * chore: update verification page
    
    * chore: update dashboard codedocs
    
    * fix: registration url

--- examples/react-spa/src/Registration.tsx
+++ examples/react-spa/src/Registration.tsx
@@ -1,50 +1,116 @@
-import React from "react"
 import {
   SelfServiceRegistrationFlow,
   SubmitSelfServiceRegistrationFlowBody,
 } from "@ory/client"
-import { useEffect, useState } from "react"
 import { UserAuthCard } from "@ory/elements"
+import { useCallback, useEffect, useState } from "react"
+import { useNavigate, useSearchParams } from "react-router-dom"
 import sdk from "./sdk"
-import { useNavigate } from "react-router-dom"
 
 export const Registration = () => {
   const [flow, setFlow] = useState<SelfServiceRegistrationFlow | null>(null)
+  const [searchParams, setSearchParams] = useSearchParams()
 
   const navigate = useNavigate()
 
-  useEffect(() => {
+  // create a new registration flow
+  const createFlow = useCallback(
+    () =>
+      sdk
+        // we don't need to specify the return_to here since we are building an SPA. In server-side browser flows we would need to specify the return_to
+        .initializeSelfServiceRegistrationFlowForBrowsers()
+        .then(({ data: flow }) => {
+          setFlow(flow)
+        })
+        // something serious went wrong so we redirect to the registration page
+        .catch((error) => {
+          console.error(error)
+          navigate("/signup", { replace: true })
+        }),
+    [],
+  )
+
+  // Get the flow based on the flowId in the URL (.e.g redirect to this page after flow initialized)
+  const getFlow = useCallback(
+    (flowId: string) =>
+      sdk
+        // the flow data contains the form fields, error messages and csrf token
+        .getSelfServiceRegistrationFlow(flowId)
+        .then(({ data: flow }) => setFlow(flow))
+        .catch((err) => {
+          console.error(err)
+          return err
+        }),
+    [],
+  )
+
+  // submit the registration form data to Ory
+  const submitFlow = (body: SubmitSelfServiceRegistrationFlowBody) => {
+    // something unexpected went wrong and the flow was not set
+    if (!flow) return navigate("/signup", { replace: true })
+
     sdk
-      .initializeSelfServiceRegistrationFlowForBrowsers()
-      .then(({ data: flow }) => {
-        setFlow(flow)
+      .submitSelfServiceRegistrationFlow(flow.id, body)
+      .then(() => {
+        // we successfully submitted the login flow, so lets redirect to the dashboard
+        navigate("/", { replace: true })
       })
-      .catch((error) => console.error(error))
+      .catch((error) => {
+        switch (error.response.status) {
+          // some user input error occurred, so we update the flow which constains UI error messages
+          case 400:
+            setFlow(error.response.data)
+            break
+          case 422:
+            // for webauthn we need to reload the flow
+            const u = new URL(error.response.data.redirect_browser_to)
+            // get new flow data based on the flow id in the redirect url
+            getFlow(u.searchParams.get("flow") || "")
+              // something unexpected went wrong and the flow was not set - redirect the user to the login page
+              .catch((err) => {
+                console.error(err)
+                navigate("/signup", { replace: true })
+              })
+            break
+          // other errors we just redirect to the registration page
+          case 410:
+          case 404:
+          default:
+            return navigate("/signup", { replace: true })
+        }
+      })
+  }
+
+  useEffect(() => {
+    // we might redirect to this page after the flow is initialized, so we check for the flowId in the URL
+    const flowId = searchParams.get("flow")
+    // the flow already exists
+    if (flowId) {
+      getFlow(flowId).catch(createFlow) // if for some reason the flow has expired, we need to get a new one
+      return
+    }
+    // we assume there was no flow, so we create a new one
+    createFlow()
   }, [])
 
+  // the flow is not set yet, so we show a loading indicator
   return flow ? (
+    // create a registration form that dynamically renders based on the flow data using Ory Elements
     <UserAuthCard
-      flow={flow}
-      flowType={"registration"}
       title={"Registration"}
+      flowType={"registration"}
+      // we always need to pass the flow to the card since it contains the form fields, error messages and csrf token
+      flow={flow}
+      // the registration card needs a way to navigate to the login page
       additionalProps={{
         loginURL: "/login",
       }}
+      // include the necessary scripts for webauthn to work
       includeScripts={true}
-      onSubmit={({ body }) => {
-        sdk
-          .submitSelfServiceRegistrationFlow(
-            flow.id,
-            body as SubmitSelfServiceRegistrationFlowBody,
-          )
-          .then(() => {
-            // we successfully submitted the login flow, so lets redirect to the dashboard
-            navigate("/", { replace: true })
-          })
-          .catch((error) => {
-            setFlow(error.response.data)
-          })
-      }}
+      // submit the registration form data to Ory
+      onSubmit={({ body }) =>
+        submitFlow(body as SubmitSelfServiceRegistrationFlowBody)
+      }
     />
   ) : (
     <div>Loading...</div>
--- examples/react-spa/src/Verification.tsx
+++ examples/react-spa/src/Verification.tsx
@@ -1,29 +1,113 @@
-import React from "react"
-import { SelfServiceVerificationFlow } from "@ory/client"
-import { useEffect, useState } from "react"
+import {
+  SelfServiceVerificationFlow,
+  SubmitSelfServiceVerificationFlowBody,
+} from "@ory/client"
 import { UserAuthCard } from "@ory/elements"
+import { useCallback, useEffect, useState } from "react"
+import { useNavigate, useSearchParams } from "react-router-dom"
 import sdk from "./sdk"
 
 export const Verification = (): JSX.Element => {
   const [flow, setFlow] = useState<SelfServiceVerificationFlow | null>(null)
+  const [searchParams, setSearchParams] = useSearchParams()
 
-  useEffect(() => {
+  const navigate = useNavigate()
+
+  // create a new verification flow
+  const createFlow = useCallback(
+    () =>
+      sdk
+        .initializeSelfServiceVerificationFlowForBrowsers()
+        // flow contains the form fields, error messages and csrf token
+        .then(({ data: flow }) => {
+          setFlow(flow)
+        })
+        // something serious went wrong so we redirect to the verification page
+        .catch((error) => {
+          console.error(error)
+          navigate("/verification", { replace: true })
+        }),
+    [],
+  )
+
+  // Get the flow based on the flowId in the URL (.e.g redirect to this page after flow initialized)
+  const getFlow = useCallback(
+    (flowId: string) =>
+      sdk
+        // the flow data contains the form fields, error messages and csrf token
+        .getSelfServiceVerificationFlow(flowId)
+        .then(({ data: flow }) => setFlow(flow))
+        .catch((err) => {
+          console.error(err)
+          return err
+        }),
+    [],
+  )
+
+  // submit the verification form data to Ory
+  const submitFlow = (body: SubmitSelfServiceVerificationFlowBody) => {
+    // something unexpected went wrong and the flow was not set
+    if (!flow) return navigate("/verification", { replace: true })
+
     sdk
-      .initializeSelfServiceVerificationFlowForBrowsers()
+      .submitSelfServiceVerificationFlow(flow.id, body)
       .then(({ data: flow }) => {
         setFlow(flow)
       })
-      .catch((error) => console.error(error))
+      .catch((error) => {
+        switch (error.response.status) {
+          // some user input error occurred, so we update the flow which constains UI error messages
+          case 400:
+            setFlow(error.response.data)
+            break
+          case 422:
+            // we might have an expired flow or the verification flow was already submitted,
+            // so we refresh it here
+            const u = new URL(error.response.data.redirect_browser_to)
+            // get new flow data based on the flow id in the redirect url
+            getFlow(u.searchParams.get("flow") || "")
+              // something unexpected went wrong and the flow was not set - redirect the user to the login page
+              .catch((err) => {
+                console.error(err)
+                navigate("/verification", { replace: true })
+              })
+            break
+          // other errors we just redirect to the registration page
+          case 410:
+          case 404:
+          default:
+            return navigate("/verification", { replace: true })
+        }
+      })
+  }
+
+  useEffect(() => {
+    // it could happen that we are redirected here with an existing flow
+    const flowId = searchParams.get("flow")
+    if (flowId) {
+      // if the flow failed to get since it could be expired or invalid, we create a new one
+      getFlow(flowId).catch(createFlow)
+      return
+    }
+    createFlow()
   }, [])
 
+  // if the flow is not set, we show a loading indicator
   return flow ? (
+    // create a new verification form with the flow data using Ory Elements
     <UserAuthCard
-      flow={flow}
+      title="Verification"
       flowType={"verification"}
+      // we always need to provide the flow data since it contains the form fields, error messages and csrf token
+      flow={flow}
+      // we want users to be able to go back to the login page from the verification page
       additionalProps={{
         loginURL: "/login",
       }}
-      title="Verification"
+      // submit the verification form data to Ory
+      onSubmit={({ body }) =>
+        submitFlow(body as SubmitSelfServiceVerificationFlowBody)
+      }
     />
   ) : (
     <div>Loading...</div>
