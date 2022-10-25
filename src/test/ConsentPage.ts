f47dbc74bed7fad531c1e75faad1b00a77e8e90c
Miłosz Szekiel <12242002+mszekiel@users.noreply.github.com>
2022-10-25 14:19:58 +0200

    test: add minimum test suit for consent page

---
+++ src/test/ConsentPage.ts
@@ -0,0 +1,37 @@
+import { UiNode } from "@ory/client"
+import { expect, Locator } from "@playwright/test"
+import { Traits } from "./types"
+import { inputNodesToRecord, isUiNode } from "./Utils"
+
+export class ConsentPage {
+  readonly locator: Locator
+  readonly formFields: Record<string, Locator> = {}
+
+  constructor(locator: Locator) {
+    this.locator = locator
+  }
+
+  async expectScopeFields(scopes: string[]) {
+    for (const scope of scopes) {
+      await expect(
+        this.locator.locator(`input[type="checkbox"][value="${scope}"]`),
+      ).toBeVisible()
+    }
+  }
+
+  async expectUris(uris: string[]) {
+    for (const uri of uris) {
+      await expect(this.locator.locator(`a[href="${uri}"]`)).toBeVisible()
+    }
+  }
+
+  async expectAllowSubmit() {
+    await expect(
+      this.locator.locator('button[value="accept"][type="submit"]'),
+    ).toBeVisible()
+  }
+
+  async submitForm() {
+    await this.locator.locator('[type="submit"]').click()
+  }
+}
