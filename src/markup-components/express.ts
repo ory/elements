13874bc02a3c815d2a22945ac9186161e9bf6d1c
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-12-14 19:13:41 +0100

    fix: express

--- src/markup-components/express.ts
+++ src/markup-components/express.ts
@@ -3,19 +3,35 @@
 
 import { assignInlineVars } from "@vanilla-extract/dynamic"
 import express, { Application, Request, Response } from "express"
+import { merge } from "lodash"
 import { oryTheme, Theme } from "../theme"
 
-export const RegisterOryElementsExpress = (
+type CreateHelpers = (
+  req: Request,
+  res: Response,
+) => {
+  theme?: Theme
+}
+
+type OryEelementsExpressRoute = (
   app: Application,
   defaultTheme: Theme,
+  createHelpers: CreateHelpers,
+) => void
+
+export const RegisterOryElementsExpress: OryEelementsExpressRoute = (
+  app: Application,
+  defaultTheme: Theme,
+  createHelpers: CreateHelpers,
 ) => {
   app.use("/theme.css", (req: Request, res: Response) => {
+    const { theme } = createHelpers(req, res)
     res.header("Content-Type", "text/css")
     res.send(
-      `body {${assignInlineVars(oryTheme, {
-        ...defaultTheme,
-        ...(req.theme && req.theme),
-      }).toString()}}`,
+      `body {${assignInlineVars(
+        oryTheme,
+        merge({}, defaultTheme, theme || {}),
+      ).toString()}}`,
     )
   })
   app.use("/", express.static("node_modules/@ory/elements/dist"))
