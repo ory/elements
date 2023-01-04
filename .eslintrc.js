8eaf517956708fbf797f75804a776dc17c69b190
Miłosz Szekiel <12242002+mszekiel@users.noreply.github.com>
2023-01-04 16:03:45 +0100

    feat: update storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,9 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true,
+    node: true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/jsx-runtime",
-    "eslint-plugin-tsdoc",
-  ],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:react/jsx-runtime", "eslint-plugin-tsdoc", "plugin:storybook/recommended"],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -21,11 +15,11 @@ module.exports = {
     ecmaVersion: 2021,
     sourceType: "module",
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   plugins: ["react", "@typescript-eslint"],
   rules: {
-    "tsdoc/syntax": "warn",
-  },
-}
+    "tsdoc/syntax": "warn"
+  }
+};
