e330c65e2e2f59639cc95ef891f90866b1e1e4cc
Misa Munde <misa.munde@gmail.com>
2023-01-19 18:33:30 +0200

    fix: remove plugin:storybook/recommended from preact/react eslintrc

--- examples/preact-spa/.eslintrc.cjs
+++ examples/preact-spa/.eslintrc.cjs
@@ -13,7 +13,6 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/jsx-runtime",
-    "plugin:storybook/recommended",
   ],
   overrides: [],
   parser: "@typescript-eslint/parser",
