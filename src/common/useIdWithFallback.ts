772ec5b0b557c88c4a07ab95e8515d589f90e865
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-07-11 13:35:48 +0200

    feat: add documentation for useIdWithFallback

--- src/common/useIdWithFallback.ts
+++ src/common/useIdWithFallback.ts
@@ -1,5 +1,11 @@
 import React from 'react'
 
+/**
+ * A function to obtain a unique ID. If react is available, this
+ * is just a wrapper for React.useId(), meaning the ID will be
+ * consistent across SSR and CSR. Otherwise, it will be a random and
+ * unique ID on every call.
+ */
 export const useIdWithFallback = () => {
   try {
     return React.useId()
