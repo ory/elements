7aa7ca8ba9e239f1f91d299ea0f88fe298a22d97
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-08-08 16:59:00 +0200

    fix: import useId behind webpacks back

--- src/common/useIdWithFallback.ts
+++ src/common/useIdWithFallback.ts
@@ -3,16 +3,24 @@
 
 import * as React from "react"
 
+const fallback = () => Math.random().toString(36).substring(2)
+
 /**
- * A function to obtain a unique ID. If react is available, this
+ * A function to obtain a unique ID. If React is available, this
  * is just a wrapper for React.useId(), meaning the ID will be
  * consistent across SSR and CSR. Otherwise, it will be a random and
  * unique ID on every call.
  */
 export const useIdWithFallback = () => {
-  try {
-    return React.useId()
-  } catch (e) {
-    return Math.random().toString(36).substring(2)
-  }
+  const useId =
+    // This is just a hacky way of optionally importing useId. If
+    // it's imported the regular way, or we even attempt to read
+    // the `useId` property of React, webpack seems to throw an
+    // error. Since `useId` isn't a hard dependency, we're fine
+    // with it not existing and this prevents the error from
+    // occurring.
+    (Object.entries(React).find(([key]) => key === "useId")?.[1] as
+      | undefined
+      | (() => string)) ?? fallback
+  return useId()
 }
