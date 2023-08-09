e7c8438a38784cce96d885ca6e8e668e459c51ab
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2023-08-09 19:06:11 +0200

    fix: oidc should show pre-filled traits and missing traits

--- src/react-components/ory/sections/oidc-section.tsx
+++ src/react-components/ory/sections/oidc-section.tsx
@@ -7,22 +7,18 @@ import { hasOidc } from "../helpers/utils"
 export const OIDCSection = (flow: SelfServiceFlow): JSX.Element | null => {
   return hasOidc(flow.ui.nodes) ? (
     <div className={gridStyle({ gap: 32 })}>
-      {filterNodesByGroups({
-        nodes: flow.ui.nodes,
-        groups: "oidc",
-        withoutDefaultGroup: true,
-      }).some((node) => node.messages.length > 0) && (
-        <div className={gridStyle({ gap: 16 })}>
-          {/*  we need other OIDC fields here such as input fields when an OIDC registration flow occured and it redirects us back to complete the missing traits */}
-          <FilterFlowNodes
-            filter={{
-              nodes: flow.ui.nodes,
-              groups: ["oidc"],
-              excludeAttributes: "submit",
-            }}
-          />
-        </div>
-      )}
+
+      <div className={gridStyle({ gap: 16 })}>
+        {/*  we need other OIDC fields here such as input fields when an OIDC registration flow occured and it redirects us back to complete the missing traits */}
+        <FilterFlowNodes
+          filter={{
+            nodes: flow.ui.nodes,
+            groups: "oidc",
+            withoutDefaultGroup: true,
+            excludeAttributes: "submit",
+          }}
+        />
+      </div>
 
       <FilterFlowNodes
         filter={{
