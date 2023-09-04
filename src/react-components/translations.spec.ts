87372ef1675f0261ec30035d8037b37b708be215
Patrik <zepatrik@users.noreply.github.com>
2023-09-04 13:40:24 +0200

    fix: german translation errors (#135)

---
+++ src/react-components/translations.spec.ts
@@ -0,0 +1,16 @@
+// Copyright © 2023 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
+import { expect, test } from "@playwright/experimental-ct-react"
+import * as supportedLanguages from "../locales"
+
+test("keys match", async () => {
+  const { en, ...otherLanguages } = supportedLanguages
+  const enKeys = Object.keys(en).sort()
+
+  for (const [language, translations] of Object.entries(otherLanguages)) {
+    await test.step(`Checking ${language}`, () => {
+      expect(Object.keys(translations).sort()).toEqual(enKeys)
+    })
+  }
+})
