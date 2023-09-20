1cab84a165bafc84f4666e8937efe818caa21579
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2023-09-20 17:03:35 +0200

    style: format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
+// Copyright © 2023 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
 module.exports = {
-  extends: [
-    'semistandard',
-    'plugins:playwright/recommended'
-  ]
-};
+  extends: ["semistandard", "plugins:playwright/recommended"],
+}
