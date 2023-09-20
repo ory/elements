41cb187cd9bf599c6282528f6ebff5bc037ac346
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2023-09-20 17:00:43 +0200

    feat: eslint config & playwright eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,6 @@
-// Copyright © 2023 Ory Corp
-// SPDX-License-Identifier: Apache-2.0
-
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:storybook/recommended",
-  ],
-  overrides: [],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    tsconfigRootDir: "./",
-    ecmaVersion: 2021,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  plugins: ["react", "@typescript-eslint", "eslint-plugin-tsdoc", "formatjs"],
-  rules: {
-    "tsdoc/syntax": "warn",
-    "formatjs/no-offset": "error",
-  },
-}
+    'semistandard',
+    'plugins:playwright/recommended'
+  ]
+};
