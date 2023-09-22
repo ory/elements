6809084da8d06c1d79d0bec2576450b5d54e7fff
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2023-09-22 14:18:22 +0200

    chore: eslint ignore specific files

--- src/markup-components/component-wrapper.tsx
+++ src/markup-components/component-wrapper.tsx
@@ -1,18 +1,18 @@
 // Copyright © 2023 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
 
-import { FunctionComponent, PropsWithChildren } from "react"
+import { FunctionComponent } from "react"
 import ReactDOMServer from "react-dom/server"
 import { IntlProvider } from "../react-components"
 import * as locales from "../locales"
 
-export interface Context {
+export type Context = {
   locale?: keyof typeof locales
 }
 
-export const ComponentWrapper = <WrapperProps extends PropsWithChildren>(
-  Component: FunctionComponent<WrapperProps>,
-  props: WrapperProps,
+export const ComponentWrapper = <Props extends {}>(
+  Component: FunctionComponent<Props>,
+  props: Props,
   { locale = "en" }: Context,
 ) => {
   return ReactDOMServer.renderToStaticMarkup(
