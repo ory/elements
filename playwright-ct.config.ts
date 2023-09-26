3d06651dc607f4aa8f87dba4f07177c79f8697de
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2023-09-26 12:22:05 +0200

    chore: relax snapshot comparison

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -23,6 +23,16 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000,
+    toHaveScreenshot: {
+      maxDiffPixelRatio: 0.05,
+    },
+  },
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
