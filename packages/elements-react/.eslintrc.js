38f53eb482ac1ebbcaea3d3dc1ff660d1b30ca0f
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2024-08-01 14:46:58 +0200

    chore: format

--- packages/elements-react/.eslintrc.js
+++ packages/elements-react/.eslintrc.js
@@ -1,27 +1,30 @@
+// Copyright © 2024 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        "../../.eslintrc.cjs",
-        'eslint:recommended',
-        'plugin:prettier/recommended',
-    ],
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-        project: __dirname + "/tsconfig.json",
-    },
-    rules: {
-        // false positive with typescript
-        "no-unused-vars": "off",
-        "@typescript-eslint/consistent-type-definitions": "off",
-        // Add your custom rules here
-    },
-    env: {
-        "jest": true
-    }
-};
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "../../.eslintrc.cjs",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    project: __dirname + "/tsconfig.json",
+  },
+  rules: {
+    // false positive with typescript
+    "no-unused-vars": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+    // Add your custom rules here
+  },
+  env: {
+    jest: true,
+  },
+}
--- packages/elements-react/src/util/clientConfiguration.ts
+++ packages/elements-react/src/util/clientConfiguration.ts
@@ -1,3 +1,6 @@
+// Copyright © 2024 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
 import { ConfigurationParameters } from "@ory/client-fetch"
 
 export type OryClientConfiguration = {
