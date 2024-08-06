6dd2bff69fc863ffd2c0433a693ae8b603523954
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2024-08-06 12:15:37 +0200

    chore: add ci

--- examples/preact-spa/playwright.config.ts
+++ examples/preact-spa/playwright.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   testDir: "./e2e",
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
+  snapshotPathTemplate: "{testDir}/__screenshots__/{testFilePath}/{arg}{ext}",
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -35,7 +36,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [["github"], ["html"], ["list"]] : "html",
+  reporter: process.env.CI
+    ? [["github"], ["html", { open: "never" }], ["list"]]
+    : "html",
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
