0f8d973242e5d71d78690d48435be379bc77bb6a
Miłosz Szekiel <12242002+mszekiel@users.noreply.github.com>
2024-09-03 16:59:32 +0200

    fix: formatting

--- examples/nextjs-spa/src/middleware.ts
+++ examples/nextjs-spa/src/middleware.ts
@@ -16,80 +16,78 @@ export const middleware = async (request: NextRequest) =>
   !["/", "/settings"].includes(new URL(request.url).pathname)
     ? NextResponse.next()
     : process.env.NEXT_PUBLIC_ORY_SDK_URL
-      ? fetch(`${process.env.NEXT_PUBLIC_ORY_SDK_URL}/sessions/whoami`, {
-          headers: {
-            cookie: request.headers.get("cookie") || "",
-          },
+    ? fetch(`${process.env.NEXT_PUBLIC_ORY_SDK_URL}/sessions/whoami`, {
+        headers: {
+          cookie: request.headers.get("cookie") || "",
+        },
+      })
+        .then((resp) => {
+          // there must've been no response (invalid URL or something...)
+          if (!resp) {
+            return NextResponse.redirect(
+              new URL(
+                `/error?error=${JSON.stringify({
+                  message:
+                    "Network error - this could be related to CORS or an incorrect URL set on the `NEXT_PUBLIC_ORY_SDK_URL` environment variable. " +
+                    "Please check out the Ory documentation for more information: https://www.ory.sh/docs/getting-started/local-development",
+                })}`,
+                request.url,
+              ),
+            )
+          }
+
+          console.log(
+            `Global Session Middleware: ${JSON.stringify(resp.json())}`,
+          )
+
+          // the user is not signed in
+          if (resp.status === 401) {
+            return NextResponse.redirect(new URL("/login", request.url))
+          }
+
+          return NextResponse.next()
         })
-          .then((resp) => {
-            // there must've been no response (invalid URL or something...)
-            if (!resp) {
-              return NextResponse.redirect(
-                new URL(
-                  `/error?error=${JSON.stringify({
-                    message:
-                      "Network error - this could be related to CORS or an incorrect URL set on the `NEXT_PUBLIC_ORY_SDK_URL` environment variable. " +
-                      "Please check out the Ory documentation for more information: https://www.ory.sh/docs/getting-started/local-development",
-                  })}`,
-                  request.url,
-                ),
-              )
-            }
-
-            console.log(
-              `Global Session Middleware: ${JSON.stringify(resp.json())}`,
+        .catch((err) => {
+          console.log(`Global Session Middleware error: ${JSON.stringify(err)}`)
+          if (!err.response)
+            return NextResponse.redirect(
+              new URL(
+                `/error?error=${JSON.stringify({
+                  message:
+                    "Network error - this could be related to CORS or an incorrect URL set on the `NEXT_PUBLIC_ORY_SDK_URL` environment variable. " +
+                    "Please check out the Ory documentation for more information: https://www.ory.sh/docs/getting-started/local-development",
+                })}`,
+                request.url,
+              ),
             )
 
-            // the user is not signed in
-            if (resp.status === 401) {
+          switch (err.response?.status) {
+            // 422 we need to redirect the user to the location specified in the response
+            case 422:
+              return NextResponse.redirect(
+                err.response.data.redirect_browser_to,
+              )
+            //return router.push("/login", { query: { aal: "aal2" } })
+            case 401:
+              // The user is not logged in, so we redirect them to the login page.
               return NextResponse.redirect(new URL("/login", request.url))
-            }
-
-            return NextResponse.next()
-          })
-          .catch((err) => {
-            console.log(
-              `Global Session Middleware error: ${JSON.stringify(err)}`,
-            )
-            if (!err.response)
+            case 404:
+              // the SDK is not configured correctly
+              // we set this up so you can debug the issue in the browser
               return NextResponse.redirect(
-                new URL(
-                  `/error?error=${JSON.stringify({
-                    message:
-                      "Network error - this could be related to CORS or an incorrect URL set on the `NEXT_PUBLIC_ORY_SDK_URL` environment variable. " +
-                      "Please check out the Ory documentation for more information: https://www.ory.sh/docs/getting-started/local-development",
-                  })}`,
-                  request.url,
-                ),
+                new URL(`/error?error=${JSON.stringify(err)}`, request.url),
               )
-
-            switch (err.response?.status) {
-              // 422 we need to redirect the user to the location specified in the response
-              case 422:
-                return NextResponse.redirect(
-                  err.response.data.redirect_browser_to,
-                )
-              //return router.push("/login", { query: { aal: "aal2" } })
-              case 401:
-                // The user is not logged in, so we redirect them to the login page.
-                return NextResponse.redirect(new URL("/login", request.url))
-              case 404:
-                // the SDK is not configured correctly
-                // we set this up so you can debug the issue in the browser
-                return NextResponse.redirect(
-                  new URL(`/error?error=${JSON.stringify(err)}`, request.url),
-                )
-              default:
-                return NextResponse.redirect(
-                  new URL(`/error?error=${JSON.stringify(err)}`, request.url),
-                )
-            }
-          })
-      : NextResponse.redirect(
-          new URL(
-            `/error?error=${JSON.stringify({
-              message: "NEXT_PUBLIC_ORY_SDK_URL env variable is not set",
-            })}`,
-            request.url,
-          ),
-        )
+            default:
+              return NextResponse.redirect(
+                new URL(`/error?error=${JSON.stringify(err)}`, request.url),
+              )
+          }
+        })
+    : NextResponse.redirect(
+        new URL(
+          `/error?error=${JSON.stringify({
+            message: "NEXT_PUBLIC_ORY_SDK_URL env variable is not set",
+          })}`,
+          request.url,
+        ),
+      )
