5e2bbc4751ab35396c3cec8fbc99961f0cccaa0c
Miłosz Szekiel <12242002+mszekiel@users.noreply.github.com>
2024-09-05 11:44:38 +0200

    chore: remove playwright tests

--- packages/elements-react/src/util/i18n/__tests__/test-components.tsx
+++
@@ -1,9 +0,0 @@
-import { formatMessage } from "../index"
-import { UiText } from "@ory/client-fetch"
-import { useIntl } from "react-intl"
-
-export const Inner = ({ uiText }: { uiText: UiText }) => {
-  const intl = useIntl()
-
-  return <div>{formatMessage(uiText, intl)}</div>
-}
