5e2432029115fd246f40d011655b58eaff69fc72
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2024-09-16 10:59:51 +0200

    fix(theme)!: scope css classes with .ory-default-theme

--- packages/elements-react/src/theme/default/components/card/index.tsx
+++ packages/elements-react/src/theme/default/components/card/index.tsx
@@ -1,4 +1,4 @@
-import { OryCardProps } from "@ory/elements-react"
+import { OryCardProps } from "../../../../components"
 import { Badge } from "./badge"
 import { DefaultCardContent } from "./content"
 import { DefaultCardFooter } from "./footer"
@@ -7,9 +7,11 @@ import { DefaultCardLogo } from "./logo"
 
 export function DefaultCard({ children }: OryCardProps) {
   return (
-    <div className="grid grid-cols-1 max-w-sm md:max-w-[480px] md:w-[480px] gap-8 bg-dialog-bg-default px-8 md:px-12 py-12 md:py-14 relative rounded-border-radius-cards border border-dialog-border-default">
-      {children}
-      <Badge />
+    <div className="ory-default-theme">
+      <div className="grid grid-cols-1 max-w-sm md:max-w-[480px] md:w-[480px] gap-8 bg-dialog-bg-default px-8 md:px-12 py-12 md:py-14 relative rounded-border-radius-cards border border-dialog-border-default">
+        {children}
+        <Badge />
+      </div>
     </div>
   )
 }
