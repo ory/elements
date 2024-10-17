506003dacf8862dd659cca77048107056b3c1f31
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-10-17 16:11:08 +0200

    chore: synchronize workspaces

--- examples/nextjs-app-router/next/middleware.ts
+++ examples/nextjs-app-router/next/middleware.ts
@@ -1,26 +1,51 @@
-import { NextResponse, type NextRequest, NextMiddleware } from "next/server"
+import { NextResponse, type NextRequest } from "next/server"
 import { rewriteUrls } from "@/next/rewrite"
 import {
   filterRequestHeaders,
   processSetCookieHeader,
 } from "@/next/router/app/utils"
-import { getSdkUrl } from "@/next/urls"
-import { Config } from "@/next/types"
+import { OryConfig } from "@/next/types"
+import { defaultOmitHeaders } from "@/next/headers"
 
-async function proxyRequest(request: NextRequest, options: Config) {
-  const baseUrl = getSdkUrl(options)
+function getProjectSdkUrl(options: OryConfig) {
+  let baseUrl = ""
 
-  const path = request.nextUrl.pathname
-  const url = new URL(path, baseUrl)
-  url.search = request.nextUrl.search
+  if (process.env.ORY_SDK_URL) {
+    baseUrl = process.env.ORY_SDK_URL
+  }
+
+  if (options.orySdkUrl) {
+    baseUrl = options.orySdkUrl
+  }
+
+  return baseUrl.replace(/\/$/, "")
+}
+
+async function proxyRequest(request: NextRequest, options: OryConfig) {
+  const match = ["/self-service", "/sessions/whoami", "/ui"]
+  if (!match.some((m) => request.nextUrl.pathname.startsWith(m))) {
+    return NextResponse.next()
+  }
+
+  const matchBaseUrl = new URL(getProjectSdkUrl(options))
+  const selfUrl = request.nextUrl.protocol + "//" + request.nextUrl.host
+
+  const upstreamUrl = request.nextUrl.clone()
+  upstreamUrl.hostname = matchBaseUrl.hostname
+  upstreamUrl.host = matchBaseUrl.host
+  upstreamUrl.protocol = matchBaseUrl.protocol
+  upstreamUrl.port = matchBaseUrl.port
 
   const requestHeaders = filterRequestHeaders(options.forwardAdditionalHeaders)
+  requestHeaders.set("Host", upstreamUrl.hostname)
 
+  // Special headers for Ory Network URL rewrites (disabled for now):
   requestHeaders.set("X-Ory-Base-URL-Rewrite", "false")
   requestHeaders.set("Ory-Base-URL-Rewrite", "false")
   requestHeaders.set("Ory-No-Custom-Domain-Redirect", "true")
 
-  const response = await fetch(url, {
+  // Fetch the upstream response
+  const upstreamResponse = await fetch(upstreamUrl.toString(), {
     method: request.method,
     headers: requestHeaders,
     body:
@@ -30,55 +55,57 @@ async function proxyRequest(request: NextRequest, options: Config) {
     redirect: "manual",
   })
 
-  const responseHeaders = new Headers()
-  response.headers.forEach((v, k) => responseHeaders.append(k, v))
+  // Delete headers that should not be forwarded
+  defaultOmitHeaders.forEach((header) => {
+    upstreamResponse.headers.delete(header)
+  })
 
-  responseHeaders.delete("location")
-  responseHeaders.delete("set-cookie")
-  if (response.headers.get("set-cookie")) {
+  // Modify cookie domain
+  if (upstreamResponse.headers.get("set-cookie")) {
     const cookies = processSetCookieHeader(
       request.nextUrl.protocol,
-      response,
+      upstreamResponse,
       options,
     )
+    upstreamResponse.headers.delete("set-cookie")
     cookies.forEach((cookie) => {
-      responseHeaders.append("Set-Cookie", cookie)
+      upstreamResponse.headers.append("Set-Cookie", cookie)
     })
   }
 
-  if (response.headers.get("location")) {
+  // Modify location header
+  if (upstreamResponse.headers.get("location")) {
     const location = rewriteUrls(
-      response.headers.get("location") || "",
-      baseUrl,
+      upstreamResponse.headers.get("location") || "",
+      matchBaseUrl.toString(),
+      selfUrl,
       options,
     )
-    responseHeaders.set("location", location)
+    upstreamResponse.headers.set("location", location)
   }
 
-  responseHeaders.delete("transfer-encoding")
-  responseHeaders.delete("content-encoding")
-  responseHeaders.delete("content-length")
-
-  let responseBuffer = Buffer.from(await response.arrayBuffer())
+  // Modify buffer
+  let modifiedBody = Buffer.from(await upstreamResponse.arrayBuffer())
   if (
-    response.headers.get("content-type")?.includes("text/") ||
-    response.headers.get("content-type")?.includes("application/json")
+    upstreamResponse.headers.get("content-type")?.includes("text/") ||
+    upstreamResponse.headers.get("content-type")?.includes("application/json")
   ) {
-    const bufferString = responseBuffer.toString("utf-8")
-    responseBuffer = Buffer.from(rewriteUrls(bufferString, baseUrl, options))
+    const bufferString = modifiedBody.toString("utf-8")
+    modifiedBody = Buffer.from(
+      rewriteUrls(bufferString, matchBaseUrl.toString(), selfUrl, options),
+    )
   }
 
-  return new NextResponse(responseBuffer, {
-    status: response.status,
-    headers: responseHeaders,
+  // Return the modified response
+  return new NextResponse(modifiedBody, {
+    headers: upstreamResponse.headers,
+    status: upstreamResponse.status,
   })
 }
 
 // This function can be marked `async` if using `await` inside
-export function createMiddleware(options: Config) {
+export function createOryMiddleware(options: OryConfig) {
   return (r: NextRequest) => {
     return proxyRequest(r, options)
   }
 }
-
-export const middlewareMatchers = ["/(self-service|ui)(.*)"]
