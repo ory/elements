30f70ec4a679040dce75d663f19ac527fc9bd530
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-10-18 12:05:17 +0200

    chore: synchronize workspaces

--- examples/nextjs-app-router/nextjs/app/index.ts
+++
@@ -1,8 +0,0 @@
-import AuthLayout from "@/nextjs/app/layout"
-import { getLoginFlow } from "@/nextjs/app/login"
-
-export { AuthLayout, getLoginFlow }
-
-export interface OryPageParams {
-  searchParams: URLSearchParams
-}
