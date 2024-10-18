c7b8f469470e62c50b36c5436cc556ef7c240947
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-10-18 16:27:57 +0200

    chore: synchronize workspaces

--- examples/nextjs-pages-router/middleware.ts
+++ examples/nextjs-pages-router/middleware.ts
@@ -1,4 +1,4 @@
-import { createOryMiddleware } from "@ory/nextjs/dist/src/server"
+import { createOryMiddleware } from "@ory/nextjs/server"
 
 // This function can be marked `async` if using `await` inside
 export const middleware = createOryMiddleware({
