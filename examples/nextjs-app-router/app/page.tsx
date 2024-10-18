6b823cf702a892c77dc8e0f32c688b6ee1fb9f1f
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-10-18 12:05:17 +0200

    chore: synchronize workspaces

--- examples/nextjs-app-router/app/page.tsx
+++ examples/nextjs-app-router/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useSession } from "@/nextjs/hooks"
+import { useSession } from "@ory/nextjs/hooks"
 import Link from "next/link"
 
 export default function RootLayout() {
--- examples/nextjs-app-router/nextjs/index.ts
+++
@@ -1,6 +0,0 @@
-import { OryConfig } from "./types"
-import { useOryConfig } from "@/nextjs/config"
-import { newFrontendClient } from "@/nextjs/sdk"
-
-export type { OryConfig }
-export { useOryConfig, newFrontendClient }
