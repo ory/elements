b44578a53f2652231e72f9bc100afe769583cd9b
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2024-10-29 15:06:43 +0100

    fix: duplicate resend button on recovery & verification

--- packages/elements-react/src/theme/default/components/form/pin-code-input.tsx
+++ packages/elements-react/src/theme/default/components/form/pin-code-input.tsx
@@ -18,25 +18,21 @@ export const DefaultPinCodeInput = ({ attributes }: OryNodeInputProps) => {
   const value = watch(name)
 
   return (
-    <div
-      className={`grid grid-flow-col grid-cols-${maxlength} gap-3 auto-cols-fr rtl:space-x-reverse`}
+    <InputOTP
+      maxLength={maxlength ?? 6}
+      onChange={handleInputChange}
+      name={name}
+      value={value}
     >
-      <InputOTP
-        maxLength={maxlength ?? 6}
-        onChange={handleInputChange}
-        name={name}
-        value={value}
-      >
-        <InputOTPGroup className="w-full space-x-2 justify-between">
-          {[...Array(elements)].map((_, index) => (
-            <InputOTPSlot
-              index={index}
-              key={index}
-              className="text-center px-1.5 py-2.5 h-10 w-11 md:w-14 md:h-12 border border-radius border-solid rounded-border-radius-forms border-forms-border-default bg-forms-bg-default"
-            />
-          ))}
-        </InputOTPGroup>
-      </InputOTP>
-    </div>
+      <InputOTPGroup className="w-full space-x-2 justify-between">
+        {[...Array(elements)].map((_, index) => (
+          <InputOTPSlot
+            index={index}
+            key={index}
+            className="text-center px-1.5 py-2.5 h-10 w-11 md:w-14 md:h-12 border border-radius border-solid rounded-border-radius-forms border-forms-border-default bg-forms-bg-default"
+          />
+        ))}
+      </InputOTPGroup>
+    </InputOTP>
   )
 }
