db3416ae7c02b4d177b405990f50675e354195fd
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-11-13 15:36:48 +0100

    chore: synchronize workspaces

--- packages/nextjs/src/app/index.ts
+++ packages/nextjs/src/app/index.ts
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 "use server"
 export { getLoginFlow } from "./login"
-export { oryAppRouterConfig } from "./config"
 
 export interface OryPageParams {
   searchParams: URLSearchParams
--- packages/nextjs/src/utils/config.ts
+++ packages/nextjs/src/utils/config.ts
@@ -1,12 +1,33 @@
 // Copyright © 2024 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
 import { OryConfig } from "../types"
-import { OryClientConfiguration } from "@ory/elements-react"
+import { isProduction } from "./sdk"
 
-export function useOryConfig(
-  sdkUrl: string,
+export function enhanceConfig(
   config: Partial<OryConfig>,
-): OryClientConfiguration {
+  forceSdkUrl?: string,
+) {
+  let sdkUrl =
+    forceSdkUrl ??
+    process.env["NEXT_PUBLIC_ORY_SDK_URL"] ??
+    process.env["ORY_SDK_URL"]
+
+  if (!forceSdkUrl && !isProduction()) {
+    if (process.env["__NEXT_PRIVATE_ORIGIN"]) {
+      sdkUrl = process.env["__NEXT_PRIVATE_ORIGIN"].replace(/\/$/, "")
+    } else if (process.env["VERCEL_URL"]) {
+      sdkUrl = `https://${process.env["VERCEL_URL"]}`.replace(/\/$/, "")
+    } else if (typeof window !== "undefined") {
+      sdkUrl = window.location.origin
+    }
+  }
+
+  if (!sdkUrl) {
+    throw new Error(
+      "Unable to determine SDK URL. Please set NEXT_PUBLIC_ORY_SDK_URL and/or ORY_SDK_URL or force the SDK URL using `useOryConfig(config, 'https://my-ory-sdk-url.com')`.",
+    )
+  }
+
   return {
     name: config.override?.applicationName ?? "Default name",
     sdk: {
