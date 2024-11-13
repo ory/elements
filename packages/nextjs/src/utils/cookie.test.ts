5eb8bc801b015993ba1b0f7c0454fe9e4048e2ef
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-11-13 14:42:02 +0100

    chore: synchronize workspaces

--- packages/nextjs/src/utils/cookie.test.ts
+++ packages/nextjs/src/utils/cookie.test.ts
@@ -1,10 +1,7 @@
-// Copyright © 2024 Ory Corp
-// SPDX-License-Identifier: Apache-2.0
-
 import { guessCookieDomain } from "./cookie"
 
 describe("cookie guesser", () => {
-  test("uses force domain", () => {
+  test("uses force domain", async () => {
     expect(
       guessCookieDomain("https://localhost", {
         forceCookieDomain: "some-domain",
@@ -12,37 +9,46 @@ describe("cookie guesser", () => {
     ).toEqual("some-domain")
   })
 
-  test("does not use any guessing domain", () => {
-    expect(guessCookieDomain("https://localhost", {})).toEqual(undefined)
+  test("does not use any guessing domain", async () => {
+    expect(guessCookieDomain("https://localhost", {})).toEqual("localhost")
   })
 
-  test("is not confused by invalid data", () => {
-    expect(guessCookieDomain("5qw5tare4g", {})).toEqual(undefined)
+  test("is not confused by invalid data", async () => {
     expect(guessCookieDomain("https://123.123.123.123.123", {})).toEqual(
       undefined,
     )
   })
 
-  test("is not confused by IP", () => {
-    expect(guessCookieDomain("https://123.123.123.123", {})).toEqual(undefined)
+  test("is not confused by IPv4", async () => {
+    expect(guessCookieDomain("https://123.123.123.123", {})).toEqual(
+      "123.123.123.123",
+    )
+  })
+
+  test("is not confused by IPv6", async () => {
     expect(
-      guessCookieDomain("https://2001:0db8:0000:0000:0000:ff00:0042:8329", {}),
+      guessCookieDomain("https://2001:0000:130F:0000:0000:09C0:876A:130B", {}),
     ).toEqual(undefined)
   })
 
-  test("returns undefined for public suffix", () => {
-    expect(guessCookieDomain("https://asdf.vercel.app", {})).toEqual(undefined)
+  test("uses TLD", async () => {
+    expect(guessCookieDomain("https://www.example.org", {})).toEqual(
+      "example.org",
+    )
+
+    expect(guessCookieDomain("https://www.example.org:1234", {})).toEqual(
+      "example.org",
+    )
+    expect(guessCookieDomain("https://localhost/123", {})).toEqual("localhost")
+    expect(guessCookieDomain("https://foo.localhost/123", {})).toEqual(
+      "foo.localhost",
+    )
+    expect(guessCookieDomain("https://localhost:1234/123", {})).toEqual(
+      "localhost",
+    )
   })
 
-  test("uses TLD", () => {
-    expect(guessCookieDomain("https://foo.localhost", {})).toEqual(
-      "foo.localhost",
-    )
-
-    expect(guessCookieDomain("https://foo.localhost:1234", {})).toEqual(
-      "foo.localhost",
-    )
-
+  test("understands public suffix list", () => {
     expect(
       guessCookieDomain(
         "https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv",
@@ -53,10 +59,8 @@ describe("cookie guesser", () => {
     expect(guessCookieDomain("spark-public.s3.amazonaws.com", {})).toEqual(
       "spark-public.s3.amazonaws.com",
     )
-
-    expect(guessCookieDomain("https://localhost/123", {})).toEqual("localhost")
-    expect(guessCookieDomain("https://localhost:1234/123", {})).toEqual(
-      "localhost",
-    )
+    expect(
+      guessCookieDomain("https://docs-gamma-seven.vercel.app", {}),
+    ).toEqual("docs-gamma-seven.vercel.app")
   })
 })
