f687cadcef2b06c005ee020d7863c3c03ec2ef71
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2024-11-27 17:40:54 +0100

    chore: fix coverage config

--- packages/elements-react/jest.config.ts
+++ packages/elements-react/jest.config.ts
@@ -11,6 +11,12 @@ export default {
     ".+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
   },
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "src/**/*.tsx",
+    "src/**/*.js",
+    "src/**/*.jsx",
+  ],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   coverageDirectory: "../../coverage/packages/elements-react",
   coveragePathIgnorePatterns: ["/node_modules/", "/dist/", ".svg"],
