e8429c733677b9fab50f9babd29a8d2e3c9a75db
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2025-01-10 14:39:50 +0100

    chore: add more tests

--- packages/elements-react/src/theme/default/components/card/footer.test.tsx
+++ packages/elements-react/src/theme/default/components/card/footer.test.tsx
@@ -5,6 +5,19 @@ import { OryProvider } from "@ory/elements-react"
 import { defaultConfiguration } from "../../../../tests/jest/test-utils"
 import { DefaultCardFooter } from "./footer"
 
+const flow = (returnTo?: string) => {
+  return {
+    request_url: `http://localhost:4455${returnTo ? "?return_to=" + returnTo : ""}`,
+    state: "",
+    ui: {
+      action: "",
+      method: "",
+      nodes: [],
+      messages: [],
+    },
+  } as unknown as LoginFlow
+}
+
 type renderProps = {
   providerProps: ComponentProps<typeof OryProvider>
 }
@@ -19,44 +32,51 @@ const renderWithContext = (
   )
 }
 
-test("login footer should render correct sign up link", () => {
+test("login footer should render return to in sign up link", () => {
   const x = renderWithContext(<DefaultCardFooter />, {
     providerProps: {
       components: null!,
       config: defaultConfiguration,
       flowType: FlowType.Login,
-      flow: {
-        request_url: "http://localhost:4455?return_to=https://ory.sh",
-        state: "",
-        ui: {
-          action: "",
-          method: "",
-          nodes: [],
-          messages: [],
-        },
-      } as unknown as LoginFlow,
+      flow: flow("https://ory.sh"),
     },
   })
 
   expect(x.container).toMatchSnapshot()
 })
 
-test("registration footer should render correct sign in link", () => {
+test("registration footer should render return to in sign in link", () => {
   const x = renderWithContext(<DefaultCardFooter />, {
     providerProps: {
       components: null!,
       config: defaultConfiguration,
       flowType: FlowType.Registration,
-      flow: {
-        request_url: "http://localhost:4455?return_to=https://ory.sh",
-        state: "",
-        ui: {
-          action: "",
-          method: "",
-          nodes: [],
-          messages: [],
-        },
-      } as unknown as LoginFlow,
+      flow: flow("https://ory.sh"),
+    },
+  })
+
+  expect(x.container).toMatchSnapshot()
+})
+
+test("login footer should render sign up link", () => {
+  const x = renderWithContext(<DefaultCardFooter />, {
+    providerProps: {
+      components: null!,
+      config: defaultConfiguration,
+      flowType: FlowType.Login,
+      flow: flow(),
+    },
+  })
+
+  expect(x.container).toMatchSnapshot()
+})
+test("registration footer should render sign in link", () => {
+  const x = renderWithContext(<DefaultCardFooter />, {
+    providerProps: {
+      components: null!,
+      config: defaultConfiguration,
+      flowType: FlowType.Registration,
+      flow: flow(),
     },
   })
 
