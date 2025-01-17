ace4c80d08e80052b4d39f3fdfdb191809976d1e
aeneasr <3372410+aeneasr@users.noreply.github.com>
2025-01-17 15:54:49 +0100

    chore: synchronize workspaces

--- packages/nextjs/src/app/index.ts
+++ packages/nextjs/src/app/index.ts
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 "use server"
 
-import { getFlow } from "./flow"
-
 export { getLoginFlow } from "./login"
 export { getRegistrationFlow } from "./registration"
 export { getRecoveryFlow } from "./recovery"
@@ -11,6 +9,5 @@ export { getVerificationFlow } from "./verification"
 export { getSettingsFlow } from "./settings"
 export { getLogoutFlow } from "./logout"
 export { getServerSession } from "./session"
-export { getFlow } from "./flow"
 
 export type { OryPageParams } from "./utils"
