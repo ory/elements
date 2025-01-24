432d46184a12a6182fbaa3fe87bb353050d710dd
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2025-01-24 07:31:01 +0100

    fix: make OIDC remove button loading state specific

--- packages/elements-react/src/theme/default/components/settings/settings-oidc.tsx
+++ packages/elements-react/src/theme/default/components/settings/settings-oidc.tsx
@@ -3,12 +3,13 @@
 
 import { OrySettingsOidcProps } from "@ory/elements-react"
 import { DefaultButtonSocial, extractProvider } from "../form/social"
-import { UiNodeInputAttributes } from "@ory/client-fetch"
+import { UiNode, UiNodeInputAttributes } from "@ory/client-fetch"
 import { DefaultHorizontalDivider } from "../form/horizontal-divider"
 import logos from "../../provider-logos"
 import Trash from "../../assets/icons/trash.svg"
 import { useFormContext } from "react-hook-form"
 import { Spinner } from "../form/spinner"
+import { useEffect, useState } from "react"
 
 export function DefaultSettingsOidc({
   linkButtons,
@@ -16,9 +17,6 @@ export function DefaultSettingsOidc({
 }: OrySettingsOidcProps) {
   const hasLinkButtons = linkButtons.length > 0
   const hasUnlinkButtons = unlinkButtons.length > 0
-  const {
-    formState: { isSubmitting },
-  } = useFormContext()
 
   return (
     <div className="flex flex-col gap-8">
@@ -41,38 +39,64 @@ export function DefaultSettingsOidc({
       )}
       {hasUnlinkButtons && hasLinkButtons ? <DefaultHorizontalDivider /> : null}
       {unlinkButtons.map((button) => {
-        const attrs = button.attributes as UiNodeInputAttributes
-        const provider = extractProvider(button.meta.label?.context) ?? ""
-        const Logo = attrs.value in logos ? logos[attrs.value] : logos.generic
-
-        return (
-          <div key={attrs.value} className="flex justify-between">
-            <div className="flex items-center gap-6">
-              <Logo size={32} />
-              <p className="text-sm font-medium text-interface-foreground-default-secondary">
-                {provider}
-              </p>
-            </div>
-            <button
-              {...attrs}
-              type="submit"
-              onClick={button.onClick}
-              disabled={isSubmitting}
-              className="relative"
-              title={`Unlink ${provider}`}
-            >
-              {isSubmitting ? (
-                <Spinner className="relative" />
-              ) : (
-                <Trash
-                  className="text-button-link-default-secondary hover:text-button-link-default-secondary-hover"
-                  size={24}
-                />
-              )}
-            </button>
-          </div>
-        )
+        if (button.attributes.node_type !== "input") {
+          return null
+        }
+        return <UnlinkRow key={button.attributes.value} button={button} />
       })}
+    </div>
+  )
+}
+
+type UnlinkRowProps = {
+  button: UiNode & { onClick: () => void }
+}
+
+function UnlinkRow({ button }: UnlinkRowProps) {
+  const [clicked, setClicked] = useState(false)
+  const {
+    formState: { isSubmitting },
+  } = useFormContext()
+  const attrs = button.attributes as UiNodeInputAttributes
+  const provider = extractProvider(button.meta.label?.context) ?? ""
+  const Logo = attrs.value in logos ? logos[attrs.value] : logos.generic
+
+  const localOnClick = () => {
+    setClicked(true)
+    button.onClick()
+  }
+
+  useEffect(() => {
+    if (!isSubmitting) {
+      setClicked(false)
+    }
+  }, [isSubmitting])
+
+  return (
+    <div key={attrs.value} className="flex justify-between">
+      <div className="flex items-center gap-6">
+        <Logo size={32} />
+        <p className="text-sm font-medium text-interface-foreground-default-secondary">
+          {provider}
+        </p>
+      </div>
+      <button
+        {...attrs}
+        type="submit"
+        onClick={localOnClick}
+        disabled={isSubmitting}
+        className="relative"
+        title={`Unlink ${provider}`}
+      >
+        {clicked ? (
+          <Spinner className="relative" />
+        ) : (
+          <Trash
+            className="text-button-link-default-secondary hover:text-button-link-default-secondary-hover"
+            size={24}
+          />
+        )}
+      </button>
     </div>
   )
 }
