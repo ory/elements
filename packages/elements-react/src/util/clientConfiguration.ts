966a4e045b63de3ee6b9d660a1bee6100b0889db
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2025-04-07 16:36:50 -0400

    chore: add more docs

--- packages/elements-react/src/util/clientConfiguration.ts
+++ packages/elements-react/src/util/clientConfiguration.ts
@@ -9,6 +9,16 @@ import { IntlContextProps } from "../context/intl-context"
 
 export type IntlConfig = IntlContextProps
 
+/**
+ * The configuration for Ory Elements.
+ *
+ * This configuration is used to customize the behavior and appearance of Ory Elements.
+ *
+ * By setting UI urls, you can override the default URLs for the login, registration, recovery, and verification flows.
+ *
+ * You can also set the name of the application, the logo URL, and the SDK configuration.
+ * By default, the name and logo are displayed in the card's header.
+ */
 export type OryClientConfiguration = {
   /**
    * An optional logo URL to display in the UI instead of the name.
@@ -16,11 +26,21 @@ export type OryClientConfiguration = {
    */
   logoUrl?: string
 
+  /**
+   * The SDK configuration.
+   * This configuration is used to set the URL of the Ory SDK and any additional options used for the SDK client.
+   */
   sdk: {
     url: string
     options?: Partial<ConfigurationParameters>
   }
 
+  /**
+   * The internationalization configuration.
+   * This configuration is used to set the locale and any additional options used for the i18n library.
+   * The locale is used to determine the language of the UI.
+   * The default locale is "en".
+   */
   intl?: IntlConfig
 } & (
   | {
