8695dedcdf2b5c2dc16e65cc71b05122a0e65484
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2025-04-11 13:18:36 -0400

    chore: remove legacy elements

--- src/react.ts
+++
@@ -1,9 +0,0 @@
-// Copyright © 2023 Ory Corp
-// SPDX-License-Identifier: Apache-2.0
-
-export * from "./ui"
-export * from "./common"
-export * from "./react-components"
-export * from "./react-components/ory/helpers/types"
-export * from "./theme"
-export * as locales from "./locales"
