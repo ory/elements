167a6e78067bda2323491d93976eef0ca1c7dfb7
hackerman <3372410+aeneasr@users.noreply.github.com>
2025-04-15 18:49:22 +0200

    chore: add organizations to storybook (#441)

---
+++ packages/elements-react/stories/components/registration/two-step/organization.stories.ts
@@ -0,0 +1,36 @@
+// Copyright © 2024 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
+import { config } from "../../../utils"
+import { LoginFlowFromJSON, RegistrationFlowFromJSON } from "@ory/client-fetch"
+import type { Meta, StoryObj } from "@storybook/react"
+import { Registration } from "../../../../src/theme/default"
+
+const meta = {
+  title: "Ory Elements/Two Step Registration/Methods/Organizations",
+  component: Registration,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof Registration>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const ShowForm: Story = {
+  args: {
+    flow: RegistrationFlowFromJSON(
+      require("$snapshots/registration/two-step/organization/initial-form.json"),
+    ),
+    config,
+  },
+}
+export const ShowOrganizationIDPs: Story = {
+  args: {
+    flow: LoginFlowFromJSON(
+      require("$snapshots/registration/two-step/organization/organization-providers.json"),
+    ),
+    config,
+  },
+}
