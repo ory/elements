0e1800a165e4e29c4790d636ff9dbf6315c474fc
aeneasr <3372410+aeneasr@users.noreply.github.com>
2025-04-17 13:03:06 +0200

    feat: add back button to settings page
    Closes #244

--- packages/elements-react/src/theme/default/components/generic/page-header.tsx
+++ packages/elements-react/src/theme/default/components/generic/page-header.tsx
@@ -1,13 +1,20 @@
 // Copyright © 2024 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
 
-import { OryPageHeaderProps, useComponents } from "@ory/elements-react"
+import {
+  OryPageHeaderProps,
+  useComponents,
+  useOryFlow,
+} from "@ory/elements-react"
 import { UserMenu } from "../ui/user-menu"
 import { useSession } from "@ory/elements-react/client"
+import { useIntl } from "react-intl"
 
 export const DefaultPageHeader = (_props: OryPageHeaderProps) => {
   const { Card } = useComponents()
   const { session } = useSession()
+  const intl = useIntl()
+  const { config } = useOryFlow()
 
   return (
     <div className="mt-16 flex max-w-screen-sm w-full md:max-w-[712px] lg:max-w-[802px] xl:max-w-[896px] flex-col gap-3 px-4">
@@ -15,6 +22,17 @@ export const DefaultPageHeader = (_props: OryPageHeaderProps) => {
         <div className="flex max-h-10 flex-1 justify-between gap-2">
           <div className="relative h-10 flex-1">
             <Card.Logo />
+            {config.project.default_redirect_url && (
+              <div className="mt-2">
+                <a href={config.project.default_redirect_url}>
+                  &#x2190;{" "}
+                  {intl.formatMessage({
+                    id: "settings.navigation-back-button",
+                    defaultMessage: "Back",
+                  })}
+                </a>
+              </div>
+            )}
           </div>
           <UserMenu session={session} />
         </div>
