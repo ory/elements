71ebbefcc18667c0ac499d96b13bd5e5f00bd895
aeneasr <3372410+aeneasr@users.noreply.github.com>
2025-04-21 16:54:32 +0200

    feat: saml settings and updated oidc snapshots

---
+++ packages/elements-react/stories/components/settings/saml.stories.ts
@@ -0,0 +1,28 @@
+// Copyright © 2024 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
+import { SettingsFlowFromJSON } from "@ory/client-fetch"
+import { Meta, StoryObj } from "@storybook/react"
+import { config } from "../../utils"
+import { SpacedSettings as Settings } from "./settings"
+
+const meta = {
+  title: "Ory Elements/Settings/Methods/SAML",
+  component: Settings,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof Settings>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const WithUnlinkProvider: Story = {
+  args: {
+    flow: SettingsFlowFromJSON(
+      require("$snapshots/settings/saml/with-provider.json"),
+    ),
+    config,
+  },
+}
