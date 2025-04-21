70a11b1670ef35bd17d8b45f7d936dd2ec3d35dd
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2025-04-21 10:32:45 -0400

    chore: fix styling

--- packages/elements-react/src/theme/default/components/generic/page-header.tsx
+++ packages/elements-react/src/theme/default/components/generic/page-header.tsx
@@ -9,36 +9,39 @@ import {
 import { UserMenu } from "../ui/user-menu"
 import { useSession } from "@ory/elements-react/client"
 import { useIntl } from "react-intl"
+import ArrowLeft from "../../assets/icons/arrow-left.svg"
 
 export const DefaultPageHeader = (_props: OryPageHeaderProps) => {
   const { Card } = useComponents()
   const { session } = useSession()
   const intl = useIntl()
-  const { config } = useOryFlow()
+  const { config, flow } = useOryFlow()
+
+  const returnUrl = flow.return_to ?? config.project.default_redirect_url
 
   return (
     <div className="mt-16 flex max-w-screen-sm w-full md:max-w-[712px] lg:max-w-[802px] xl:max-w-[896px] flex-col gap-3 px-4">
       <div className="flex flex-col gap-12">
-        <div className="flex max-h-10 flex-1 justify-between gap-2">
-          <div className="relative h-10 flex-1">
-            <Card.Logo />
-            {config.project.default_redirect_url && (
-              <div className="mt-2">
-                <a
-                  data-testid={"ory/screen/settings/default_redirect_url"}
-                  href={config.project.default_redirect_url}
-                >
-                  &#x2190;{" "}
-                  {intl.formatMessage({
-                    id: "settings.navigation-back-button",
-                    defaultMessage: "Back",
-                  })}
-                </a>
-              </div>
-            )}
-          </div>
+        <div className="flex max-h-10 flex-1 justify-between gap-2 items-center">
+          <Card.Logo />
           <UserMenu session={session} />
         </div>
+
+        {returnUrl && (
+          <div>
+            <a
+              data-testid={"ory/screen/settings/back-button"}
+              href={returnUrl}
+              className="inline-flex gap-2 items-center"
+            >
+              <ArrowLeft />{" "}
+              {intl.formatMessage({
+                id: "settings.navigation-back-button",
+                defaultMessage: "Back",
+              })}
+            </a>
+          </div>
+        )}
       </div>
     </div>
   )
