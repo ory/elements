8ff1461982e2673fef65e8572271bf2dab5e6410
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2025-04-22 15:58:51 -0400

    fix: add test id to header messages

--- packages/elements-react/src/theme/default/components/card/header.tsx
+++ packages/elements-react/src/theme/default/components/card/header.tsx
@@ -1,11 +1,19 @@
 // Copyright © 2024 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
 
-import { useComponents, useOryFlow } from "@ory/elements-react"
+import { messageTestId, useComponents, useOryFlow } from "@ory/elements-react"
 import { useCardHeaderText } from "../../utils/constructCardHeader"
 import { DefaultCurrentIdentifierButton } from "./current-identifier-button"
 
-function InnerCardHeader({ title, text }: { title: string; text?: string }) {
+function InnerCardHeader({
+  title,
+  text,
+  messageId,
+}: {
+  title: string
+  text?: string
+  messageId?: string
+}) {
   const { Card } = useComponents()
   return (
     <header className="flex flex-col gap-8 antialiased">
@@ -14,7 +22,10 @@ function InnerCardHeader({ title, text }: { title: string; text?: string }) {
         <h2 className="text-lg font-semibold leading-normal text-interface-foreground-default-primary">
           {title}
         </h2>
-        <p className="leading-normal text-interface-foreground-default-secondary">
+        <p
+          className="leading-normal text-interface-foreground-default-secondary"
+          {...(messageId ? messageTestId({ id: messageId }) : {})}
+        >
           {text}
         </p>
         <DefaultCurrentIdentifierButton />
@@ -25,7 +36,12 @@ function InnerCardHeader({ title, text }: { title: string; text?: string }) {
 
 export function DefaultCardHeader() {
   const context = useOryFlow()
-  const { title, description } = useCardHeaderText(context.flow.ui, context)
+  const { title, description, messageId } = useCardHeaderText(
+    context.flow.ui,
+    context,
+  )
 
-  return <InnerCardHeader title={title} text={description} />
+  return (
+    <InnerCardHeader title={title} text={description} messageId={messageId} />
+  )
 }
