37508ba380649e1bb0818190c82fda65472dc5c4
aeneasr <3372410+aeneasr@users.noreply.github.com>
2025-04-23 11:57:06 +0200

    chore: synchronize workspaces

--- examples/nextjs-pages-router/pages/auth/registration.tsx
+++ examples/nextjs-pages-router/pages/auth/registration.tsx
@@ -1,7 +1,7 @@
 // Copyright © 2024 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
 "use client"
-import { Registration } from "@ory/elements-react/theme"
+import { DefaultCardLayout, Registration } from "@ory/elements-react/theme"
 import { useRegistrationFlow } from "@ory/nextjs/pages"
 import { enhanceOryConfig } from "@ory/nextjs"
 import "@ory/elements-react/theme/styles.css"
@@ -16,7 +16,7 @@ export default function RegistrationPage() {
   }
 
   return (
-    <div className="pt-4">
+    <DefaultCardLayout>
       <Registration
         flow={flow}
         config={enhanceOryConfig(config)}
@@ -24,6 +24,6 @@ export default function RegistrationPage() {
           Card: {},
         }}
       />
-    </div>
+    </DefaultCardLayout>
   )
 }
