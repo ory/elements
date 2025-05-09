3f18b4d29782b8eb08773d22e9cc8e88072a664d
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2025-05-09 17:18:09 -0400

    chore: stabilize stories (#522)

--- packages/elements-react/.storybook/preview.tsx
+++ packages/elements-react/.storybook/preview.tsx
@@ -23,6 +23,24 @@ const preview: Preview = {
       },
     },
 
+    mockAddonConfigs: {
+      globalMockData: [
+        {
+          // An array of mock objects which will add in every story
+          url: "http://localhost:0000/self-service/logout/browser",
+          method: "GET",
+          status: 201,
+          response: {
+            logout_url:
+              "http://localhost:0000/self-service/logout?token=ory_lo_XXXXXXXXX",
+            logout_token: "ory_lo_XXXXXXXXX",
+          },
+        },
+      ],
+      ignoreQueryParams: true, // Whether or not to ignore query parameters globally
+      refreshStoryOnUpdate: true, // This property re-renders the story if there's any data changes
+      disableUsingOriginal: false, // This property disables the toggle (on/off) option to use the original endpoint
+    },
     a11y: {
       config: {},
       /*
@@ -38,7 +56,7 @@ const preview: Preview = {
   },
 }
 
-const withI18next: Decorator = (Story, context) => {
+const withProviders: Decorator = (Story, context) => {
   const { locale } = context.globals
 
   return (
@@ -59,7 +77,7 @@ const withI18next: Decorator = (Story, context) => {
 }
 
 // export decorators for storybook to wrap your stories in
-export const decorators = [withI18next, mockDateDecorator]
+export const decorators = [withProviders, mockDateDecorator]
 
 export const globalTypes = {
   locale: {
