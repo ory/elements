9d796e4fa9cb5a71c2f7b2628a4c01b750c38241
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2025-09-02 17:18:54 -0400

    fix: add scoping to CSS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -135,6 +135,7 @@ const config = tseslint.config([
       // enable all recommended rules to report an error
       ...eslintPluginBetterTailwindcss.configs["recommended-error"].rules,
       "better-tailwindcss/enforce-consistent-line-wrapping": "off",
+      "better-tailwindcss/no-unregistered-classes": ["error", { ignore: ["ory-elements"] }]
     },
     settings: {
       "better-tailwindcss": {
