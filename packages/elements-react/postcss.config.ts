3ad0b0f3c6d2659bcd8c45e0f2d38ea391cfa754
Arno DUBOIS <arno.du@orange.fr>
2025-09-03 02:17:02 +0200

    feat(elements): add prefixed CSS
    This should fix #566 by prepending a ".ory" before all selectors, meaning that every component can be wrapped in something that contains the class "ory" to apply the style.
    
    Signed-off-by: Arno DUBOIS <arno.du@orange.fr>

--- packages/elements-react/postcss.config.ts
+++ packages/elements-react/postcss.config.ts
@@ -3,5 +3,5 @@
 
 module.exports = {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-require-imports
-  plugins: [require("@tailwindcss/postcss")()],
+  plugins: [require("@tailwindcss/postcss")(), require("postcss-scope")(".ory-elements")],
 }
--- packages/elements-react/src/theme/default/components/card/index.tsx
+++ packages/elements-react/src/theme/default/components/card/index.tsx
@@ -19,10 +19,12 @@ import { DefaultCurrentIdentifierButton } from "./current-identifier-button"
  */
 export function DefaultCard({ children }: OryCardProps) {
   return (
-    <div className="flex w-full flex-1 items-start justify-center font-sans-default sm:w-[480px] sm:max-w-[480px] sm:items-center">
-      <div className="relative grid w-full grid-cols-1 gap-8 border-b border-form-border-default bg-form-background-default px-8 py-12 sm:rounded-cards sm:border sm:px-12 sm:py-14">
-        {children}
-        <Badge />
+    <div className="ory-elements">
+      <div className="flex w-full flex-1 items-start justify-center font-sans-default sm:w-[480px] sm:max-w-[480px] sm:items-center">
+        <div className="relative grid w-full grid-cols-1 gap-8 border-b border-form-border-default bg-form-background-default px-8 py-12 sm:rounded-cards sm:border sm:px-12 sm:py-14">
+          {children}
+          <Badge />
+        </div>
       </div>
     </div>
   )
